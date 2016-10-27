50bcc9614a646c47d7a76527d5ff3ce3abf4a688
Jono Forbes <jono@unity3d.com>
2016-10-27 15:00:44 -0700

    Add SpanTool beginnings with span system from Carte Blanche

---
+++ Tools/SpanTool/SpanTool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CB.VossetSystem;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.VR;
+using UnityEngine.VR.Tools;
+using UnityEngine.VR.Utilities;
+using UnityEngine.InputNew;
+
+[MainMenuItem("Span", "Create", "Create a span")]
+public class SpanTool : MonoBehaviour, ITool, IStandardActionMap, IRay
+{
+	const float m_DistanceToSpawnNewSpan = 1f;
+
+	public Standard standardInput
+	{
+		get; set;
+	}
+
+	public Transform rayOrigin
+	{
+		get; set;
+	}
+
+	void Update()
+	{
+		if (standardInput.action.wasJustPressed)
+		{
+			if (rayOrigin)
+			{
+				new SpanGroup(rayOrigin.position + rayOrigin.forward * m_DistanceToSpawnNewSpan);
+			}
+		}
+	}
+}
