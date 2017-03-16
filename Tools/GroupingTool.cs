52d8f136aff78d017e80dc0ec0ffdcaaeffbcc62
Amir Ebrahimi <amir@unity3d.com>
2017-03-16 13:18:37 -0700

    Add GroupingTool

---
+++ Tools/GroupingTool.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.EditorVR.Utilities;
+using UnityEngine;
+using UnityEngine.InputNew;
+
+namespace UnityEditor.Experimental.EditorVR.Tools
+{
+	sealed class GroupingTool : MonoBehaviour, ITool, IStandardActionMap, IUsesRayOrigin
+	{
+		public Transform rayOrigin { get; set; }
+		
+		public void ProcessInput(ActionMapInput input, ConsumeControlDelegate consumeControl)
+		{
+			var standardInput = (Standard)input;
+			if (standardInput.action.wasJustPressed)
+			{
+			}
+		}
+	}
+}
+#endif
