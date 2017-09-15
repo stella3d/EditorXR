700276118dfa03bcce88ea0e1c884a479a5ca088
Dylan Urquidi <dylanu@unity3d.com>
2017-09-15 00:00:48 -0700

    Add initial member structure to ProxyAnimator

--- Scripts/Proxies/ProxyAnimator.cs
+++ Scripts/Proxies/ProxyAnimator.cs
@@ -1,15 +1,23 @@
 ﻿#if UNITY_EDITOR || UNITY_EXR_RUNTIME
 using UnityEngine;
 
-public class ProxyAnimator : MonoBehaviour {
+public class ProxyAnimator : MonoBehaviour
+{
+	[SerializeField]
+	MeshRenderer[] m_InputElements;
+
+	[SerializeField]
+	MeshRenderer[] m_Bindings;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 }
