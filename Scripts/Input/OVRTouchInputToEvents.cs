9a394093745f613bf716900ede1b03210b45e4fa
Matt Schoen <schoen@unity3d.com>
2017-09-25 17:03:25 -0700

    WIP commit trying to get axis mapping to work

--- Scripts/Input/OVRTouchInputToEvents.cs
+++ Scripts/Input/OVRTouchInputToEvents.cs
@@ -1,10 +1,14 @@
 ﻿#if UNITY_EDITOR && UNITY_2017_2_OR_NEWER
 using System;
+using System.Collections.Generic;
 using UnityEditor.Experimental.EditorVR;
 using UnityEngine;
+using UnityEngine.Experimental.Input;
 using UnityEngine.InputNew;
 using UnityEngine.VR;
 using UnityEngine.XR;
+using UnityEngineInternal.Input;
+using GenericControlEvent = UnityEngine.InputNew.GenericControlEvent;
 using InputTracking = UnityEngine.XR.InputTracking;
 
 [assembly: OptionalDependency("OVRInput", "ENABLE_OVR_INPUT")]
@@ -29,7 +33,7 @@ namespace UnityEditor.Experimental.EditorVR.Input
 		Vector3[] m_LastPositionValues = new Vector3[k_ControllerCount];
 		Quaternion[] m_LastRotationValues = new Quaternion[k_ControllerCount];
 
-		public void Update()
+		void Update()
 		{
 			var isActive = false;
 			var names = Input.GetJoystickNames();
@@ -43,7 +47,7 @@ namespace UnityEditor.Experimental.EditorVR.Input
 					m_JoystickIndices[i] = joystickIndex;
 					for (var j = 0; j < JoystickInputToEvents.axisCount; j++)
 					{
-						m_AxisNames[i, j] = string.Format("Analog{0}_Joy{1}", j + 1, joystickIndex);
+						m_AxisNames[i, j] = string.Format("Analog{0}_Joy{1}", j + 1, joystickIndex + 1);
 					}
 				}
 			}
@@ -59,81 +63,81 @@ namespace UnityEditor.Experimental.EditorVR.Input
 			{
 				int deviceIndex = hand == VRInputDevice.Handedness.Left ? 3 : 4;
 				var controller = (int)hand;
-				SendAxisEvents(controller, deviceIndex);
+				//SendAxisEvents(controller, deviceIndex);
 				var node = hand == VRInputDevice.Handedness.Left ? XRNode.LeftHand : XRNode.RightHand;
 				SendTrackingEvents(node, controller, deviceIndex);
-				SendButtonEvents(m_JoystickIndices[controller], deviceIndex);
+				//SendButtonEvents(m_JoystickIndices[controller], deviceIndex);
 			}
 		}
 
-		void OnGUI()
-		{
-			GUILayout.BeginHorizontal();
-			for (var i = 0; i < Input.GetJoystickNames().Length; i++)
-			{
-				GUILayout.BeginVertical();
-				var max = JoystickInputToEvents.axisCount + 1;
-				for (var j = 1; j < max; j++)
-				{
-					var axisName = string.Format("Analog{0}_Joy{1}", j, i + 1);
-					GUILayout.Label(string.Format("{0}: {1:f2}", axisName, Input.GetAxis(axisName)));
-				}
-				GUILayout.EndVertical();
+		//void OnGUI()
+		//{
+		//	GUILayout.BeginHorizontal();
+		//	for (var i = 0; i < Input.GetJoystickNames().Length; i++)
+		//	{
+		//		GUILayout.BeginVertical();
+		//		var max = JoystickInputToEvents.axisCount + 1;
+		//		for (var j = 1; j < max; j++)
+		//		{
+		//			var axisName = string.Format("Analog{0}_Joy{1}", j, i + 1);
+		//			GUILayout.Label(string.Format("{0}: {1:f2}", axisName, Input.GetAxis(axisName)));
+		//		}
+		//		GUILayout.EndVertical();
 
-				GUILayout.BeginVertical();
-				var first = (int)KeyCode.Joystick1Button0 + i * 20;
-				var last = (int)KeyCode.Joystick1Button19 + i * 20;
+		//		GUILayout.BeginVertical();
+		//		var first = (int)KeyCode.Joystick1Button0 + i * 20;
+		//		var last = (int)KeyCode.Joystick1Button19 + i * 20;
 
-				for (var j = 0; j <= last - first; j++)
-				{
-					var buttonName = string.Format("Button {0}_Joy{1}", j, i + 1);
-					GUILayout.Toggle(Input.GetKey((KeyCode)(j + first)), string.Format("{0}", buttonName));
-				}
-				GUILayout.EndVertical();
-			}
-			GUILayout.EndHorizontal();
-		}
+		//		for (var j = 0; j <= last - first; j++)
+		//		{
+		//			var buttonName = string.Format("Button {0}_Joy{1}", j, i + 1);
+		//			GUILayout.Toggle(Input.GetKey((KeyCode)(j + first)), string.Format("{0}", buttonName));
+		//		}
+		//		GUILayout.EndVertical();
+		//	}
+		//	GUILayout.EndHorizontal();
+		//}
 
-		void SendAxisEvents(int controller, int deviceIndex)
-		{
-			for (var axis = 0; axis < JoystickInputToEvents.axisCount; ++axis)
-			{
-				var VRIndex = AxisIndexToVRIndex(axis);
-				if (VRIndex == -1)
-					continue;
+		//void SendAxisEvents(int controller, int deviceIndex)
+		//{
+		//	for (var axis = 0; axis < JoystickInputToEvents.axisCount; ++axis)
+		//	{
+		//		var VRIndex = AxisIndexToVRIndex(axis);
+		//		if (VRIndex == -1)
+		//			continue;
 
-				var value = Input.GetAxis(m_AxisNames[controller, axis]);
-				if (Mathf.Approximately(m_LastAxisValues[controller, axis], value))
-					continue;
+		//		var value = Input.GetAxis(m_AxisNames[controller, axis]);
+		//		if (Mathf.Approximately(m_LastAxisValues[controller, axis], value))
+		//			continue;
 
-				var inputEvent = InputSystem.CreateEvent<GenericControlEvent>();
-				inputEvent.deviceType = typeof(VRInputDevice);
-				inputEvent.deviceIndex = deviceIndex;
-				inputEvent.controlIndex = VRIndex;
-				inputEvent.value = value;
+		//		var inputEvent = InputSystem.CreateEvent<GenericControlEvent>();
+		//		inputEvent.deviceType = typeof(VRInputDevice);
+		//		inputEvent.deviceIndex = deviceIndex;
+		//		inputEvent.controlIndex = VRIndex;
+		//		inputEvent.value = value;
 
-				m_LastAxisValues[controller, axis] = inputEvent.value;
+		//		m_LastAxisValues[controller, axis] = inputEvent.value;
 
-				InputSystem.QueueEvent(inputEvent);
-			}
-		}
+		//		InputSystem.QueueEvent(inputEvent);
+		//	}
+		//}
 
-		static int AxisIndexToVRIndex(int axisIndex)
-		{
-			switch (axisIndex)
-			{
-				case 10:
-					return (int)VRInputDevice.VRControl.Trigger2;
-				case 11:
-					return (int)VRInputDevice.VRControl.Trigger2;
-				case 12:
-					return (int)VRInputDevice.VRControl.Trigger1;
-				case 13:
-					return (int)VRInputDevice.VRControl.Trigger1;
-				default:
-					return -1;
-			}
-		}
+		//static int AxisIndexToVRIndex(int axisIndex)
+		//{
+		//	switch (axisIndex)
+		//	{
+		//		case 10:
+		//			return (int)VRInputDevice.VRControl.Trigger2;
+		//		case 11:
+		//			return (int)VRInputDevice.VRControl.Trigger2;
+		//		case 12:
+		//			return (int)VRInputDevice.VRControl.Trigger1;
+		//		case 13:
+		//			return (int)VRInputDevice.VRControl.Trigger1;
+		//		default:
+		//			return -1;
+		//	}
+		//}
 
 		void SendTrackingEvents(XRNode node, int controller, int deviceIndex)
 		{
@@ -155,23 +159,23 @@ namespace UnityEditor.Experimental.EditorVR.Input
 			InputSystem.QueueEvent(inputEvent);
 		}
 
-		static void SendButtonEvents(int joystickIndex, int deviceIndex)
-		{
-			var first = KeyCode.Joystick1Button0 + joystickIndex * 20;
-			var last = KeyCode.Joystick1Button19 + joystickIndex * 20;
+		//static void SendButtonEvents(int joystickIndex, int deviceIndex)
+		//{
+		//	var first = KeyCode.Joystick1Button0 + joystickIndex * 20;
+		//	var last = KeyCode.Joystick1Button19 + joystickIndex * 20;
 
-			for (var i = 0; i <= last - first; i++)
-			{
-				var VRIndex = ButtonIndexToVRIndex(i);
-				if (VRIndex == -1)
-					continue;
+		//	for (var i = 0; i <= last - first; i++)
+		//	{
+		//		var VRIndex = ButtonIndexToVRIndex(i);
+		//		if (VRIndex == -1)
+		//			continue;
 
-				if (Input.GetKeyDown(i + first))
-					SendButtonEvent(deviceIndex, VRIndex, 1.0f);
-				if (Input.GetKeyUp(i + first))
-					SendButtonEvent(deviceIndex, VRIndex, 0.0f);
-			}
-		}
+		//		if (Input.GetKeyDown(i + first))
+		//			SendButtonEvent(deviceIndex, VRIndex, 1.0f);
+		//		if (Input.GetKeyUp(i + first))
+		//			SendButtonEvent(deviceIndex, VRIndex, 0.0f);
+		//	}
+		//}
 
 		static int ButtonIndexToVRIndex(int buttonIndex)
 		{
@@ -186,16 +190,25 @@ namespace UnityEditor.Experimental.EditorVR.Input
 			}
 		}
 
-		static void SendButtonEvent(int deviceIndex, int controlIndex, float value)
-		{
-			var inputEvent = InputSystem.CreateEvent<GenericControlEvent>();
-			inputEvent.deviceType = typeof(VRInputDevice);
-			inputEvent.deviceIndex = deviceIndex;
-			inputEvent.controlIndex = controlIndex;
-			inputEvent.value = value;
+		//static void SendButtonEvent(int deviceIndex, int controlIndex, float value)
+		//{
+		//	var inputEvent = InputSystem.CreateEvent<GenericControlEvent>();
+		//	inputEvent.deviceType = typeof(VRInputDevice);
+		//	inputEvent.deviceIndex = deviceIndex;
+		//	inputEvent.controlIndex = controlIndex;
+		//	inputEvent.value = value;
 
-			InputSystem.QueueEvent(inputEvent);
-		}
+		//	Dictionary<int, float> vals;
+		//	if (!NativeInputEventManager.values.TryGetValue(deviceIndex, out vals))
+		//	{
+		//		vals = new Dictionary<int, float>();
+		//		NativeInputEventManager.values[deviceIndex] = vals;
+		//	}
+
+		//	vals[inputEvent.controlIndex] = inputEvent.value;
+
+		//	InputSystem.QueueEvent(inputEvent);
+		//}
 	}
 }
 #endif
