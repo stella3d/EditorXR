3436add7a8fe9e5adfd934b212f8bafa5988b357
Dylan Urquidi <dylanu@unity3d.com>
2017-11-12 17:39:17 -0800

    Add new IAnimateTransition interface; utliized in TooltipUI, avoiding coroutines for UI animation value goal-seeking

---
+++ Scripts/Interfaces/Capability/IAnimateTransition.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+namespace UnityEditor.Experimental.EditorVR
+{
+    /// <summary>
+    /// Gives decorated class the ability to be updated in a uniform manner
+    /// </summary>
+    public interface IAnimateTransition
+    {
+        /// <summary>
+        /// Normalized value representing the transition progress, 0-1
+        /// </summary>
+        float currentTransitionAmount { get; }
+
+        /// <summary>
+        /// Target value towards which the currentTransitionAmount should be incremented towards
+        /// </summary>
+        float targetTransitionAmount { get; }
+
+        /// <summary>
+        /// Bool representing the completion status of the transition
+        /// </summary>
+        bool complete { get; }
+
+        /// <summary>
+        /// Ticks/updates the animated transition towards the target transition amount
+        /// </summary>
+        /// <param name="deltaUpdateAmount">The delta-time based amount with which to tick an animated transition</param>
+        void Update(float deltaUpdateAmount);
+    }
+}
+#endif
