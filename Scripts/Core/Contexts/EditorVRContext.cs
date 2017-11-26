03f0edd56a7707d2d55f601d820f64d9088a1bcc
Dylan Urquidi <dylanu@unity3d.com>
2017-11-26 02:44:48 -0800

    Add copying of scene camera settings and FX support to EditorVRContext

--- Scripts/Core/Contexts/EditorVRContext.cs
+++ Scripts/Core/Contexts/EditorVRContext.cs
@@ -7,6 +7,18 @@ using UnityEngine.XR;
 
 namespace UnityEditor.Experimental.EditorVR.Core
 {
+    public struct ContextSettings
+    {
+        public bool copySceneCameraSettings;
+        public bool supportCameraFX;
+
+        public ContextSettings(bool copySceneCameraSettings, bool supportCameraFX)
+        {
+            this.copySceneCameraSettings = copySceneCameraSettings;
+            this.supportCameraFX = supportCameraFX;
+        }
+    }
+
     [CreateAssetMenu(menuName = "EditorVR/EditorVR Context")]
     class EditorVRContext : ScriptableObject, IEditingContext
     {
@@ -14,23 +26,33 @@ namespace UnityEditor.Experimental.EditorVR.Core
         float m_RenderScale = 1f;
 
         [SerializeField]
+        bool m_CopySceneCameraSettings = true;
+
+        [SerializeField]
+        bool m_SupportCameraFX = true;
+
+        [SerializeField]
         internal List<MonoScript> m_DefaultToolStack;
 
         EditorVR m_Instance;
+
+        public ContextSettings CopySceneCameraSettingsAndFX { get { return new ContextSettings(m_CopySceneCameraSettings, m_SupportCameraFX); } }
 
         public void Setup()
         {
             EditorVR.defaultTools = m_DefaultToolStack.Select(ms => ms.GetClass()).ToArray();
             m_Instance = ObjectUtils.CreateGameObjectWithComponent<EditorVR>();
-
             XRSettings.eyeTextureResolutionScale = m_RenderScale;
         }
 
         public void Dispose()
         {
-            m_Instance.Shutdown(); // Give a chance for dependent systems (e.g. serialization) to shut-down before destroying
-            ObjectUtils.Destroy(m_Instance.gameObject);
-            m_Instance = null;
+            if (m_Instance)
+            {
+                m_Instance.Shutdown(); // Give a chance for dependent systems (e.g. serialization) to shut-down before destroying
+                ObjectUtils.Destroy(m_Instance.gameObject);
+                m_Instance = null;
+            }
         }
     }
 }
